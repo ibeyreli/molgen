686642f6ed11bf0930f10da10a914dc2ed72a78f
ibeyreli <ilaydabeyreli@gmail.com>
2021-11-23 21:17:23 +0300

    data_parser.py

--- data_parse.py
+++ data_parse.py
@@ -8,7 +8,7 @@ import os, sys
 import numpy as np
 import pandas as pd
 
-RAW_FILE_DIF = "data.csv"
+RAW_FILE_DIF = "E:\\BILKENT_CS\\CS585\\molgen"
 
-raw_file = pd.read_csv(RAW_FILE_DIF)
+raw_file = pd.read_csv(os.path.join(RAW_FILE_DIF,"data.csv"))
 print(raw_file.head(2))
