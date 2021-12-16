7f372eaf66457455fcce0cd7de142d25804648b8
ibeyreli <ilayda.beyreli@bilkent.edu.tr>
2021-12-16 18:34:53 +0000

    json

--- data_parse.py
+++ data_parse.py
@@ -4,9 +4,9 @@ Data Parser for Deep Molecule Generator
 author: ilayda beyreli kokundu
 date 03/11/2021
 """
-import re
+import re, json
 import os, sys
-import pickle
+
 import numpy as np
 import pandas as pd
 
@@ -14,13 +14,18 @@ import networkx as nx
 import pysmiles as ps
 
 #RAW_FILE_DIF = "E:\\BILKENT_CS\\CS585" # Windows
-RAW_FILE_DIF = "\mnt\molgen_data" # Neo - Ubuntu
+RAW_FILE_DIF = "/mnt/ilayda/molgen_data" # Neo - Ubuntu
 
-def molecule_counter(s, d=None):
-    if d is None: d=dict()
-    
-    r = re.split(r'(\d+)|(?=[A-Z])', s)
-    
+def molecule_counter(s, d=None, e=False):
+    if d is None:
+        d=dict()
+    try:
+        r = re.split(r'(\d+)|(?=[A-Z])', s)
+    except TypeError:
+        # print("Problematic string!", s)
+        e = True 
+        return d, e
+
     for i in range(len(r)):
         if r[i] is None or r[i] == '': continue
         
@@ -37,7 +42,7 @@ def molecule_counter(s, d=None):
             else:
                 d[r[i]] = c
 
-    return d
+    return d, e
      
 
 raw_file = pd.read_csv(os.path.join(RAW_FILE_DIF,"data.csv"), sep=";")
@@ -48,26 +53,50 @@ raw_file = pd.read_csv(os.path.join(RAW_FILE_DIF,"data.csv"), sep=";")
 
 input = raw_file.values[:,-2]
 # INPUT  : (2105464,) ['C41H69NO14' 'C17H14Cl2N4O']
-
+# print("INPUT:", input)
 target = raw_file.values[:,-1]
 # TARGET : (2105464,) ['CCC(=O)O[C@H]1[C@H](C)O[C@@H](O[C@@H]2[C@@H](C)C(=O)O[C@H](C)[C@H](C)[C@H](OC(=O)CC)[C@@H](C)C(=O)[C@]3(CO3)C[C@H](C)[C@H](O[C@@H]3O[C@H](C)C[C@H](N(C)C)[C@H]3O)[C@H]2C)C[C@@H]1OC'
 #                      'COc1ccc(Cl)cc1-c1cc(Nc2ccc(Cl)cc2)nc(N)n1']
-
+print("TARGET:", target.shape)
 # Find the maximum possible number of each atom in the entire set
+
 counts = dict()
-#for molecule in input:
-#    counts = molecule_counter(molecule, counts)
-#
-#with open("counts.pkl", "wb") as fout:
-#    pickle. dump(counts, fout)
+l = [] # the list for ok molecules
+"""
+for (i,molecule) in enumerate(input):
+    counts, e = molecule_counter(molecule, counts)
+    if not e: l.append(True)
+    else: l.append(False)
+    if i % 100000 == 0:
+        print("At molecule", i)
 
-counts = pickle.load("counts.pkl")
+with open("counts.json", "w") as fout:
+    json.dump(counts, fout)
+with open("indices.json", "w") as fout:
+    json.dump(l, fout)
+"""
+with open("counts.json", "r") as fin:
+    counts = json.load(fin)
+with open("indices.json", "r") as fin:
+    l = json.load(fin)
 
-n = max(counts.values())
+print("Data set of ", len(l), "molecules")
+
+input = input[l]
+target = target[l]
+
+n = sum(counts.values())
 features = np.zeros((target.shape[0], len(counts.keys())))
 data = []
 for (i, smiles) in enumerate(target):
+    print(i, smiles)
     molecule = ps.read_smiles(smiles, explicit_hydrogen=True)
     A = nx.adjacency_matrix(molecule).todense()
-    print(A)
+    print(A, molecule.nodes(data='element'))
+    atoms, _ = molecule_counter(input[i])
+    for key in atoms.keys():
+        j = list(counts.keys()).index(key)
+        features[i,j] = atoms[key]
+
+
 
